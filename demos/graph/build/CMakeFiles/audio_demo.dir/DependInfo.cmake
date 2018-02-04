c87e04e955db24892b846cd27bc5d95e5124b476
cbrpnk <cbrpnk@users.noreply.github.com>
2018-02-03 21:11:20 -0500

    update

---
+++ demos/graph/build/CMakeFiles/audio_demo.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/cbrpnk/projects/et/et/audio/backends/jack_backend.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/audio/backends/jack_backend.cc.o"
+  "/home/cbrpnk/projects/et/et/audio/backends/portaudio_backend.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/audio/backends/portaudio_backend.cc.o"
+  "/home/cbrpnk/projects/et/et/audio/engine.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/audio/engine.cc.o"
+  "/home/cbrpnk/projects/et/et/audio/modules/module.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/audio/modules/module.cc.o"
+  "/home/cbrpnk/projects/et/et/audio/modules/oscillator.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/audio/modules/oscillator.cc.o"
+  "/home/cbrpnk/projects/et/et/dsp/oscillator.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/dsp/oscillator.cc.o"
+  "/home/cbrpnk/projects/et/et/math/functions.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/functions.cc.o"
+  "/home/cbrpnk/projects/et/et/math/mat2.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/mat2.cc.o"
+  "/home/cbrpnk/projects/et/et/math/mat3.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/mat3.cc.o"
+  "/home/cbrpnk/projects/et/et/math/mat4.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/mat4.cc.o"
+  "/home/cbrpnk/projects/et/et/math/vec2.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/vec2.cc.o"
+  "/home/cbrpnk/projects/et/et/math/vec3.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/vec3.cc.o"
+  "/home/cbrpnk/projects/et/et/math/vec4.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/home/cbrpnk/projects/et/et/math/vec4.cc.o"
+  "/home/cbrpnk/projects/et/demos/graph/main.cc" "/home/cbrpnk/projects/et/demos/graph/build/CMakeFiles/audio_demo.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ demos/graph/main.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <unistd.h>
+#include "et/graph.hpp"
+
+using namespace Et::Graph;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
